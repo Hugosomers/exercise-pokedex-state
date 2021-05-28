286dba3db68ec8eece0168fa95d2ac75976044eb
¨Hugosomers¨ <hugosomersla@gmail.com>
2021-05-27 21:21:36 -0300

    Exercicio 1 feito

--- src/App.js
+++ src/App.js
@@ -3,13 +3,32 @@ import './App.css';
 import pokemons from './data';
 import Pokedex from './Pokedex';
 
-function App() {
-  return (
-    <div className="App">
-      <h1> Pokedex </h1>
-      <Pokedex pokemons={pokemons} />
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor() {
+    super()
+    this.nextPoke = this.nextPoke.bind(this);
 
+    this.state = {
+      click: 0,
+    }
+  }
+
+  nextPoke() {
+    this.setState(actual => ({
+      click: actual.click >= pokemons.length - 1 ? 0 : actual.click + 1,
+    }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1> Pokedex </h1>
+        <Pokedex pokemon={pokemons[this.state.click]} />
+        <button onClick={ this.nextPoke }>Próximo {this.state.click}</button>
+        <button style={{backgroundColor:'red', color: 'white'}}>Fire</button>
+        <button style={{backgroundColor:'blue', color: 'white'}}>Psychic</button>
+      </div>
+    );
+  }
+}
 export default App;
