e8989ecb6515a68a79222160077d3317dfff3be6
¨Hugosomers¨ <hugosomersla@gmail.com>
2021-05-28 10:29:51 -0300

    Exercicio 2 feito

--- src/App.js
+++ src/App.js
@@ -7,26 +7,36 @@ class App extends React.Component {
   constructor() {
     super()
     this.nextPoke = this.nextPoke.bind(this);
+    this.selectElementPoke = this.selectElementPoke.bind(this);
 
     this.state = {
       click: 0,
+      pokemons: pokemons.filter(pokemon => pokemon.type === 'Fire'),
     }
   }
 
   nextPoke() {
     this.setState(actual => ({
-      click: actual.click >= pokemons.length - 1 ? 0 : actual.click + 1,
+      click: actual.click >= this.state.pokemons.length - 1 ? 0 : actual.click + 1,
     }));
+  }
+
+  selectElementPoke(tipo) {
+    const element = pokemons.filter(pokemon => pokemon.type === tipo);
+    this.setState({
+      click: 0,
+      pokemons: element,
+    })
   }
 
   render() {
     return (
       <div className="App">
         <h1> Pokedex </h1>
-        <Pokedex pokemon={pokemons[this.state.click]} />
-        <button onClick={ this.nextPoke }>Próximo {this.state.click}</button>
-        <button style={{backgroundColor:'red', color: 'white'}}>Fire</button>
-        <button style={{backgroundColor:'blue', color: 'white'}}>Psychic</button>
+        <Pokedex pokemon={this.state.pokemons[this.state.click]} />
+        <button onClick={this.nextPoke}>Próximo {this.state.click}</button>
+        <button style={{backgroundColor:'red', color: 'white'}} onClick={() => this.selectElementPoke('Fire')}>Fire</button>
+        <button style={{backgroundColor:'blue', color: 'white'}} onClick={() => this.selectElementPoke('Psychic')}>Psychic</button>
       </div>
     );
   }
