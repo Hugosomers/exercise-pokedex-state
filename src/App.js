c426fa7ebceb3d4e03b6731843c04dfc0274f1b9
¨Hugosomers¨ <hugosomersla@gmail.com>
2021-05-28 13:42:11 -0300

    Exercicio 3 bônus feito

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import pokemons from './data';
 import Pokedex from './Pokedex';
+import Button from './button'
 
 class App extends React.Component {
   constructor() {
@@ -32,6 +33,7 @@ class App extends React.Component {
 
   selectAllPoke() {
     this.setState({
+      click: 0,
       pokemons: pokemons,
     })
   }
@@ -41,10 +43,11 @@ class App extends React.Component {
       <div className="App">
         <h1> Pokedex </h1>
         <Pokedex pokemon={this.state.pokemons[this.state.click]} />
-        <button onClick={this.nextPoke}>Próximo {this.state.click}</button>
-        <button style={{backgroundColor:'red', color: 'white'}} onClick={() => this.selectElementPoke('Fire')}>Fire</button>
-        <button style={{backgroundColor:'blue', color: 'white'}} onClick={() => this.selectElementPoke('Psychic')}>Psychic</button>
-        <button onClick={this.selectAllPoke}>All</button>
+        <Button event={this.nextPoke} text={'Próximo'}/>
+        <Button event={() => this.selectElementPoke('Fire')} text={'Fire'}/>
+        <Button event={() => this.selectElementPoke('Psychic')} text={'Psychic'}/>
+        <Button event={this.selectAllPoke} text={'All'}/>
+        
       </div>
     );
   }
